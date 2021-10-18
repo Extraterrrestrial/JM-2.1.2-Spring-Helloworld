118182c2f4d4dd99510cb7a56b4f16c6acb5862f
Yuri Kriger <yuri.kriger@gmail.com>
2021-10-18 22:32:34 +0600

    App change

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+//        HelloWorld bean =
+//                (HelloWorld) applicationContext.getBean("helloworld");
+//        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1 == bean2);
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1 == cat2);
     }
 }
