ed8861fdc25170317244854f6f13659d4b5e7112
Yuri Kriger <yuri.kriger@gmail.com>
2021-10-18 22:30:01 +0600

    Initial commit

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,20 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+ 
+    @Bean(name="helloworld")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope(scopeName = "prototype")
+    public Cat getCat(){
+        return new Cat();
+    }
+}
